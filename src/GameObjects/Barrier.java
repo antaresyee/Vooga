ba37ef971c4d3f735c5b9baa3896fbe16613dc82
James Pagliuca <jamespagliuca@gmail.com>
2012-03-31 15:15:48 -0400

    added isMyObject methods to factories

--- src/GameObjects/Barrier.java
+++ src/GameObjects/Barrier.java
@@ -13,6 +13,8 @@ public class Barrier extends GameObject {
 	
 	public class BarrierFactory extends GameObjectFactory{
 		
+		private String myName = "barrier";
+		
 		public BarrierFactory(double x, double y, BufferedImage image){
 			myX = x;
 			myY = y;
@@ -22,6 +24,11 @@ public class Barrier extends GameObject {
 		@Override
 		public GameObject makeObject() {
 			return new Barrier(myX, myY, myImage);
+		}
+
+		@Override
+		public boolean isMyObject(String name) {
+			return myName.equals(name);
 		}
 		
 	}
--- src/GameObjects/Player.java
+++ src/GameObjects/Player.java
@@ -16,6 +16,8 @@ public class Player extends GameObject {
 	
 	public class PlayerFactory extends GameObjectFactory{
 		
+		private String myName = "player";
+		
 		public PlayerFactory(double x, double y, BufferedImage image){
 			myX = x;
 			myY = y;
@@ -25,6 +27,11 @@ public class Player extends GameObject {
 		@Override
 		public GameObject makeObject() {
 			return new Player(myX, myY, myImage);
+		}
+
+		@Override
+		public boolean isMyObject(String name) {
+			return myName.equals(name);
 		}
 		
 	}
