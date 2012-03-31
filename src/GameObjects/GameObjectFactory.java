e20a08df34a95eb32e73e9e2a12f3acd2964eb66
James Pagliuca <jamespagliuca@gmail.com>
2012-03-31 16:13:32 -0400

    made initResources

--- src/GameObjects/GameObjectFactory.java
+++ src/GameObjects/GameObjectFactory.java
@@ -7,9 +7,12 @@ public abstract class GameObjectFactory {
 	protected BufferedImage myImage;
 	protected double myX;
 	protected double myY;
+	protected String myName;
 
 	public abstract GameObject makeObject();
 	
-	public abstract boolean isMyObject(String name);
+	public boolean isMyObject(String name) {
+		return myName.equals(name);
+	}
 
 }
--- src/game/TopDownDemo.java
+++ src/game/TopDownDemo.java
@@ -1,14 +1,47 @@
 package game;
+import gameObjects.Barrier;
+import gameObjects.GameObjectFactory;
+import gameObjects.Player;
+
 import java.awt.Graphics2D;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+import levelLoadSave.LevelLoader;
 
 import com.golden.gamedev.Game;
+import com.golden.gamedev.GameObject;
+import com.golden.gamedev.object.SpriteGroup;
 
 
 public class TopDownDemo extends Game {
+	
+	private Player myPlayer;
+	private SpriteGroup playerGroup;
+	private SpriteGroup barrierGroup;
 
 	@Override
 	public void initResources() {
-		// TODO Auto-generated method stub
+		LevelLoader l = new LevelLoader();
+		String player = "player";
+		String barrier = "barrier";
+		try {
+			List<GameObjectFactory> factories = l.load("savedLevel.json");
+			for (GameObjectFactory f : factories){
+				if (f.isMyObject(player)){
+					myPlayer = (Player) f.makeObject();
+					playerGroup.add(myPlayer);
+				}
+				if (f.isMyObject(barrier)){
+					Barrier b = (Barrier) f.makeObject();
+					barrierGroup.add(b);
+				}
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
 		
 	}
 
