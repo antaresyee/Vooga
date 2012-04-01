4cef2f3efd3eff04cd241bc5aacccccad9e3ae72
Hao Sun <oddsun@gmail.com>
2012-04-01 19:11:37 -0400

    partly done

---
+++ src/levelEditor/LevelGUI.java
@@ -0,0 +1,87 @@
+package levelEditor;
+
+import gameObjects.GameObject;
+
+import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import com.golden.gamedev.Game;
+import com.golden.gamedev.object.background.TileBackground;
+
+public class LevelGUI extends Game{
+	
+	private final String BLACK = "resource/Black.png";
+	private List<GameObject> myObjects;
+	private TileBackground tilesBack;
+	private BufferedImage[] tileImages;
+	private String[] imageNames;
+	private int[][] tiles;
+	private int black;
+	private int row;
+	private int col;
+	
+	
+	public LevelGUI(BufferedImage[] tileImages, String[] imageNames, int row, int col)
+	{
+		myObjects = new ArrayList<GameObject>();
+		this.row=row;
+		this.col=col;
+		this.imageNames = imageNames;
+		black = tileImages.length+1;
+		tiles = new int[row][col];
+		for(int i=0; i<row;i++)
+			for(int j=0; j<col;j++)
+				tiles[i][j]=black;
+		this.tileImages = new BufferedImage[black];
+		for(int i =0; i < black -1; i++)
+		{
+			this.tileImages[i] = tileImages[i];
+		}
+		this.tileImages[black-1]=getImage(BLACK);
+		tilesBack = new TileBackground(tileImages,tiles);
+	}
+
+
+	@Override
+	public void initResources() {
+		// TODO Auto-generated method stub
+	}
+
+
+	@Override
+	public void render(Graphics2D pen) {
+		// TODO Auto-generated method stub
+		tilesBack.render(pen);
+	}
+
+
+	@Override
+	public void update(long elapsedTime) {
+		// TODO Auto-generated method stub
+		if(click())
+		{
+			int r = getMouseY()/row;
+			int c = getMouseX()/col;
+			int option = JOptionPane.showOptionDialog(new JFrame(), "Choose object to place", "Options", JOptionPane.YES_NO_CANCEL_OPTION,JOptionPane.PLAIN_MESSAGE, null, imageNames, imageNames[0]);
+			tiles[r][c]=option;
+			tilesBack.setTiles(tiles);
+		}
+		
+		if(keyPressed(KeyEvent.VK_CONTROL) && keyPressed(KeyEvent.VK_S))
+		{
+			for(int i = 0; i<row; i++)
+				for(int j = 0; j<col; j++)
+				{
+					GameObject go = (GameObject) Class.forName(imageNames[tiles[i][j]]).newInstance();
+					
+					myObjects.add(Class.forName(imageNames[tiles[i][j]]).newInstance());
+				}
+		}
+	}
+}
