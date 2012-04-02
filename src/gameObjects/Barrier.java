2e2ab40fdfe14578e58878aef874eb2e45d82c06
Antares Yee <antaresyee@gmail.com>
2012-04-02 19:50:24 -0400

    refactored GameObject and GameObjectFactory classes

--- src/gameObjects/Barrier.java
+++ src/gameObjects/Barrier.java
@@ -1,25 +1,28 @@
 package gameObjects;
-import java.awt.image.BufferedImage;
 
 
 
 public class Barrier extends GameObject {
-
-	public Barrier(){}
 	
     public Barrier(double x, double y, String path){
-        makeObj(x,y,path);
+        myX = x;
+        myY = y;
+        myImgPath = path;
+        myName = "Barrier";
+        setLocation(myX,myY);
     }
     
-    public String getPath()
+    public String getImgPath()
     {
-    	return pathToImage;
+    	return myImgPath;
     }
     
 
     public static class BarrierFactory extends GameObjectFactory{
     	
-    	public BarrierFactory(){myName = "Barrier";}
+    	public BarrierFactory(){
+    	    myName = "Barrier";
+    	}
     	
     	public void setFactory(double x, double y, String path){
     		super.myName = "Barrier";
@@ -43,21 +46,4 @@ public class Barrier extends GameObject {
         }
 
     }
-
-	@Override
-	public void makeObj(double x, double y, String path) {
-		// TODO Auto-generated method stub
-		myX = x;
-        myY = y;
-        pathToImage = path;
-        myName = "Barrier";
-        setLocation(myX,myY);
-    }
-
-//	@Override
-//	public void setImage(BufferedImage image) {
-//		// TODO Auto-generated method stub
-//		myImage = image;
-//	}
-
 }
--- src/gameObjects/GameObject.java
+++ src/gameObjects/GameObject.java
@@ -1,26 +1,13 @@
 package gameObjects;
-import java.awt.image.BufferedImage;
 
 import com.golden.gamedev.object.Sprite;
 
 public abstract class GameObject extends Sprite {
 
 	protected String myName;
-	protected String pathToImage;
-    protected BufferedImage myImage;
+	protected String myImgPath;
     protected double myX;
     protected double myY;
-    protected int width, height;
-    
-    public GameObject(){}
-
-    public abstract void makeObj(double x, double y, String path);
-    public abstract String getPath();
-    public void setWidthHeight(int w, int h)
-    {
-    	width=w;
-    	height=h;
-    }
-//    public abstract void setImage(BufferedImage image);
+    public abstract String getImgPath();
 
 }
