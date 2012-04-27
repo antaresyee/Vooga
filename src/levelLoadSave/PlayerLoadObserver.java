0edb5ae3229abc6d008413103b370df7a8bc9ecf
Antares Yee <antaresyee@gmail.com>
2012-04-26 21:36:40 -0400

    weapon integration functionality

--- src/levelLoadSave/PlayerLoadObserver.java
+++ src/levelLoadSave/PlayerLoadObserver.java
@@ -7,7 +7,15 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
+import weapons.DamagingProjectile;
+import weapons.Projectile;
+import weapons.ShotPattern;
+import weapons.SinglePattern;
+import weapons.UnlimitedGun;
+import weapons.Weapon;
+
 import game.TopDownDemo;
+import gameObjects.Enemy;
 import gameObjects.GameObject;
 import gameObjects.Player;
 
@@ -21,11 +29,14 @@ import com.golden.gamedev.object.SpriteGroup;
 
 public class PlayerLoadObserver extends LoadObserver {
     SpriteGroup myPlayerGroup;
+    SpriteGroup myProjectileGroup;
+    
     TopDownDemo myGame;
 
-    public PlayerLoadObserver(SpriteGroup playerGroup, TopDownDemo game) {
+    public PlayerLoadObserver(SpriteGroup playerGroup, SpriteGroup projectileGroup, TopDownDemo game) {
         super.myType = "Player";
         myPlayerGroup = playerGroup;
+        myProjectileGroup = projectileGroup;
         myGame = game;
     }
 
@@ -34,14 +45,20 @@ public class PlayerLoadObserver extends LoadObserver {
         System.out.println("entered player object loaded");
         myPlayerGroup.add(go);
         myGame.setPlayer((Player) go);
+        
+        Projectile p = new DamagingProjectile("resources/enemy.png",myProjectileGroup,1);
+        ShotPattern s = new SinglePattern(-3);
+        Weapon w = new UnlimitedGun(300,p,s);
+        Enemy e = (Enemy) go;
+        e.addWeapon(w);
 
         //set sprite image
         try {
             File imgSrc = new File(go.getImgPath());
             BufferedImage sourceImage = ImageIO.read(imgSrc);
             go.setImage(sourceImage);
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (IOException e1) {
+            e1.printStackTrace();
         }
         
 
